29b59cfcedb3bc695fd10060f500082c019ad8bd
Balazs Koskoci <balazs.koskoci@logmein.com>
2018-11-16 16:53:32 +0100

    Add GET users endpoint

--- models/user.js
+++ models/user.js
@@ -3,8 +3,8 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
-    email: DataTypes.STRING
-  }, {});
+    email: DataTypes.STRING,
+  }, {timestamps: false});
   User.associate = function(models) {
     // associations can be defined here
   };
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var models = require('../models');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  models.User.findAll().then(projects => res.json(projects))
 });
 
 module.exports = router;
